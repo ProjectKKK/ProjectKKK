d850dad11ebe7cb45ea36c32792624be8db138d8
haru0611 <haruharu180@gmail.com>
2016-10-23 09:37:22 +0000

    view update

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020132448) do
+ActiveRecord::Schema.define(version: 20161023085421) do
 
   create_table "comments", force: :cascade do |t|
-    t.string   "commmenter"
+    t.string   "commenter"
     t.text     "body"
     t.integer  "post_id"
     t.datetime "created_at", null: false
